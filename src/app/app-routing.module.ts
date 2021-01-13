2751ef97d528a8a768d35e251767bdff2631c53f
Rohith <rohith.chetla@dwpractice.net>
2021-01-13 11:58:08 +0530

    Updated build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'login', pathMatch:'prefix'},
+  {path:'',loadChildren:()=>import('./layout/layout.module').then(m=>m.LayoutModule)},
+  {path:'login', loadChildren:()=>import('./login/login.module').then(m=>m.LoginModule)},
+  {path:'registration', loadChildren:()=>import('./registration/registration.module').then(m=>m.RegistrationModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
+import { HomeComponent } from './layout/home/home.component';
+import { Example1Component } from './layout/example1/example1.component';
+import { NavbarComponent } from './layout/component/navbar/navbar.component';
+import { FooterComponent } from './layout/component/footer/footer.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    PageNotFoundComponent,
+    ServerErrorComponent,
+    HomeComponent,
+    Example1Component,
+    NavbarComponent,
+    FooterComponent,
+    LayoutComponent,
   ],
   imports: [
     BrowserModule,
