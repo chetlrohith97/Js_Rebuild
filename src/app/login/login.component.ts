cc1ab42d7e35860c095ae9dafb44c3eeaca4432d
chetlarohith97 <you@example.com>
2021-02-24 20:33:31 +0530

    added password icon

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,6 +14,7 @@ export class LoginComponent implements OnInit {
   remember_me = 'remember_me';
   data!: any;
   loading = false;
+  fieldTextType?: boolean;
   constructor(
     private router: Router,
     private authService: AuthService,
@@ -21,6 +22,10 @@ export class LoginComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {}
+
+  toggleFieldTextType() {
+    this.fieldTextType = !this.fieldTextType;
+  }
   loginUser(v: any) {
     const user = {
       UserName: this.userName,
